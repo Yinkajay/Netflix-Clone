ab977df78dfa25faad6fef41f73f59694a1a2e40
Adeyinka Junaid <yinkajunn@gmail.com>
2023-01-23 00:24:26 +0100

    Finish profile Login Page

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -14,3 +14,4 @@ export default ProfileCard
 // background-image:url(http://occ-0-300-1168.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbV2URr-qEYOrESG0qnP2787XsIxWTMBh7QfJwyqYxMAVFNyiXAqFeu16gI8yTxg3kLwF2mUDKmZGfwBEDd7722xskhYwAMwsBBe.png?r=bd7)
 
 
+// background-image:url(http://occ-0-300-1168.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdi6oYb6DJb1LndQWckgaXvrqqP3QURNa8Xngiq7wwFwDtWIg1otEuYKVxbYwS9c5clAI1_Bn7DuljwcvhfUzQu2_Y2v9y5P6t_n.png?r=e6e"
--- src/components/ProfilesPage.jsx
+++ src/components/ProfilesPage.jsx
@@ -3,22 +3,36 @@ import styles from './ProfilesPage.module.css'
 
 const ProfilesPage = () => {
     return (
-        <>
-            <div>
-                <h1>
-                    Manage Profiles:
-                </h1>
-                <div>
-                    <ProfileCard 
+        <div className={styles['profile-login-container']}>
+            <h1>
+                Manage Profiles:
+            </h1>
+            <div className={styles['profiles-container']}>
+                <ProfileCard
                     img={'http://occ-0-300-1168.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbV2URr-qEYOrESG0qnP2787XsIxWTMBh7QfJwyqYxMAVFNyiXAqFeu16gI8yTxg3kLwF2mUDKmZGfwBEDd7722xskhYwAMwsBBe.png?r=bd7'}
-                    profileName = 'Yinka'
+                    profileName='Yinka'
+                />
+                <ProfileCard
+                    img={'http://occ-0-300-1168.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdi6oYb6DJb1LndQWckgaXvrqqP3QURNa8Xngiq7wwFwDtWIg1otEuYKVxbYwS9c5clAI1_Bn7DuljwcvhfUzQu2_Y2v9y5P6t_n.png?r=e6e'}
+                    profileName='Banks'
+                />
+                <ProfileCard
+                    img={'http://occ-0-300-1168.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbV2URr-qEYOrESG0qnP2787XsIxWTMBh7QfJwyqYxMAVFNyiXAqFeu16gI8yTxg3kLwF2mUDKmZGfwBEDd7722xskhYwAMwsBBe.png?r=bd7'}
+                    profileName='Jake'
+                />
+                                    <ProfileCard
+                        img={'http://occ-0-300-1168.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbV2URr-qEYOrESG0qnP2787XsIxWTMBh7QfJwyqYxMAVFNyiXAqFeu16gI8yTxg3kLwF2mUDKmZGfwBEDd7722xskhYwAMwsBBe.png?r=bd7'}
+                        profileName='Yinka'
                     />
-                </div>
-                <button>
-                    Done
-                </button>
+                                        <ProfileCard
+                        img={'http://occ-0-300-1168.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbV2URr-qEYOrESG0qnP2787XsIxWTMBh7QfJwyqYxMAVFNyiXAqFeu16gI8yTxg3kLwF2mUDKmZGfwBEDd7722xskhYwAMwsBBe.png?r=bd7'}
+                        profileName='Yinka'
+                    />
             </div>
-        </>
+            <button>
+                Done
+            </button>
+        </div>
     )
 }
 
